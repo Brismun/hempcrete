b4a5becae8e1cab722c6ab0bdef59d6af2e1d82b
Brismun <96andrei.dumirescu@gmail.com>
2023-02-18 20:32:47 +0100

    resize hotfix

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=" bg-slate-500 max-w-full h-fit min-w-[280px] text-2xl">
+    <footer className="bg-slate-500 max-w-full h-fit min-w-[270px] text-2xl">
       <div className="flex flex-col md:flex-row md:justify-between items-center gap-4 relative p-4">
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <img
             src="https://loremflickr.com/640/360"
             alt=""
-            className="max-w-[200px]
-          max-h-[200px]"
+            className="max-w-[200px] max-h-[200px]"
           />
           <div className="flex justify-center items-center gap-4">
             <a href="">FB</a>
@@ -17,7 +16,7 @@ const Footer = () => {
             <a href="">MAIL</a>
           </div>
         </div>
-        <div className="flex flex-col justify-center text-center md:mr-4 md:absolute md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 md:bottom-4">
+        <div className="flex flex-col justify-center text-center md:mx-4 md:absolute md:left-1/2 md:bottom-0 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
           <a href="">Home</a>
           <a href="">AboutUs</a>
           <a href="">Gallery</a>
@@ -26,7 +25,7 @@ const Footer = () => {
         <div>
           <a href="">Contact</a>
         </div>
-        <div className="md:absolute md:left-1/2 md:bottom-0 md:transform md:-translate-x-1/2 md:-translate-y-1/2 ">
+        <div className="md:absolute md:left-1/2 md:bottom-0 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
           CopyRight 2022
         </div>
       </div>
